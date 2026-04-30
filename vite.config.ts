cc375b184130117133072955f86e5079803357be
pengbowen <pengbowen@benagen.com>
2026-04-30 10:28:40 +0800

    fix(vite): 添加server.host配置并统一代码风格
    将单引号改为双引号以保持代码风格一致，并添加server.host配置以解决开发服务器访问问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: './',
-})
+  plugins: [vue(), vueDevTools()],
+  base: "./",
+  // 配置代理
+  server: {
+    host: true,
+  },
+});
